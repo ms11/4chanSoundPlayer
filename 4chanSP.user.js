6916df12efc60beda9940c8da767b7b9f2b731d3
ms11 <msanyi11@freemail.hu>
2012-08-26 14:49:00 +0200

    fix indentation

--- 4chanSP.user.js
+++ 4chanSP.user.js
@@ -118,11 +118,11 @@ function loadAll(file,isUrl,cb) {
 		},onprogress, file);
 	}else{
 		for(var i = 0; i < file.length;i++){
-		var reader = new FileReader();
-		reader.onload = function() {
-		loadAllWithFooter(this.result,"",cb);
-		};
-		reader.readAsArrayBuffer(file[i]);
+			var reader = new FileReader();
+			reader.onload = function() {
+				loadAllWithFooter(this.result,"",cb);
+			};
+			reader.readAsArrayBuffer(file[i]);
 		}
 	}
 }
